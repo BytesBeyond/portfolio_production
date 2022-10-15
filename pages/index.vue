cf70106666203a736c29af9f44324549fc059c46
HridaySarma <hridaysarma8@gmail.com>
2022-10-15 20:27:03 +0530

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    {{ filtersList }}
+  </div>
+</template>
+
+<script>
+import {useFiltersStore} from '~/store/filters'
+import {defineComponent} from "vue/types/v3-define-component";
+
+export default defineComponent({
+  setup() {
+    const filtersStore = useFiltersStore()
+    const filtersList = filtersStore.filtersList
+
+    return {filtersList}
+  },
+})
+</script>
